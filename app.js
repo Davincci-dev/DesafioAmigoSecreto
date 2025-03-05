1efa9ca573ac0fdc26bddddaf450caa342ed0a77
Davincci-dev <davinccidev@gmail.com>
2025-03-05 08:52:55 -0500

    Mostrando el Amigo secreto en pantalla
    Mostramos el resultado de sortear el amigo secreto en pantalla usando el texto del boton de sortear amigo

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigo = "";
 let listaAmigos =[];
+function asignarTextoElemento(elemento,texto) {
+    let titulo = document.querySelector(elemento);
+    titulo.innerHTML = texto;
+    return;
+}
+asignarTextoElemento("p","Sortear amigo");
 //Creamos la lista de amigos desde los amigos que introduce el usuario
 function agregarAmigo(){
     let amigoDeUsuario = document.getElementById("amigo").value;
@@ -13,6 +19,7 @@ function agregarAmigo(){
 function sortearAmigo(amigo){
     amigo = listaAmigos[Math.floor(Math.random()*listaAmigos.length)];
     console.log(amigo);
+    asignarTextoElemento("p",`Tu Amigo secreto es: ${amigo}`);
 }
 
 function limpiarCaja(){
