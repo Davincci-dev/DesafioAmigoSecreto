a721b82e102e76e02eede349e2d82f6459c6dc57
Davincci-dev <davinccidev@gmail.com>
2025-03-05 05:14:50 -0500

    agregando funcionalidades a la App
    Capturamos los amigos introducidos por el usuario y los incluimos en una lista, limpiamos la caja de texto al añadirlo a la lista, luego le damos funcionalidad al boton de sortear el amigo

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigo = "";
+let listaAmigos =[];
+//Creamos la lista de amigos desde los amigos que introduce el usuario
+function agregarAmigo(){
+    let amigoDeUsuario = document.getElementById("amigo").value;
+    listaAmigos.push(amigoDeUsuario);
+    console.log(listaAmigos);
+    limpiarCaja();
+}
+
+//Sorteamos un nombre de la lista de amigos
+function sortearAmigo(amigo){
+    amigo = listaAmigos[Math.floor(Math.random()*listaAmigos.length)];
+    console.log(amigo);
+}
+
+function limpiarCaja(){
+    document.querySelector("#amigo").value = "";
+}
